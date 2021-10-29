4f9c21e38963bba0a431421eee6270f9ca324874
AmitSheth9 <amit.sheth9@gmail.com>
2021-10-28 22:26:41 -0700

    added echo, red, green routes, but test not passing due to spacing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,5 +6,25 @@ describe('app routes', () => {
     const res = await request(app).get('/');
     expect(res.text).toEqual('hi');
   });
+  it('route /echo post request body', async() => {
+    const res = await request(app).post('/echo');
+    console.log(res);
+    expect(res.text).toEqual('test=test');
+  });
+  it('route /red gets html red', async() => {
+    const res = await request(app).get('/red');
+    console.log(res);
+    expect(res.text).toEqual(`<!DOCTYPE html>
+    <html lang="en"><head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>red</title>
+</head>
+<body>
+    <h1>red</h1>
+
+</body></html>`);
+  });
 });
 
