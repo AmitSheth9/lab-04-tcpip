d03495c593591c1db608cebb87d6078806812a65
AmitSheth9 <amit.sheth9@gmail.com>
2021-10-28 21:19:29 -0700

    first route passed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,9 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
-
+  it('route / gets plain text hi', async() => {
+    const res = await request(app).get('/');
+    expect(res.text).toEqual('hi');
+  });
 });
 
