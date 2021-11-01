072872b6c1325a16c1477896f4fedeadc21430cc
AmitSheth9 <amit.sheth9@gmail.com>
2021-10-31 19:24:46 -0700

    all tests passing with functions written for allroutes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,24 +7,21 @@ describe('app routes', () => {
     expect(res.text).toEqual('hi');
   });
   it('route /echo post request body', async() => {
-    const res = await request(app).post('/echo');
-    console.log(res);
-    expect(res.text).toEqual('test=test');
+    const res = await request(app).post('/echo').send('test');
+    expect(res.text).toEqual('test');
   });
   it('route /red gets html red', async() => {
     const res = await request(app).get('/red');
-    console.log(res);
-    expect(res.text).toEqual(`<!DOCTYPE html>
-    <html lang="en"><head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>red</title>
-</head>
-<body>
-    <h1>red</h1>
-
-</body></html>`);
+    expect(res.text).toEqual('<h1>red</h1>');
   });
+  it('route /green gets html green', async() => {
+    const res = await request(app).get('/green');
+    expect(res.text).toContain('<h1>green</h1>');
+  });
+  it('route /blue gets html blue', async() => {
+    const res = await request(app).get('/blue');
+    expect(res.text).toContain('<h1>blue</h1>');
+  });
+
 });
 
